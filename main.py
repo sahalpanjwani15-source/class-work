70cf2447a56213f48faacc70d774493a8fdd55ec
sahalpanjwani15-source <sahalpanjwani15@gmail.com>
2026-08-17 22:14:21 +0500

    data types

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ print(4)
 #next line after print
 print("helloPython \n")
 print("hello",456)
+#class 3 data types
+a = 2
+b = 6.3
+c = "8"
+d = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+name = input("enter your name")
+print("hello",name)
