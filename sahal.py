31e60ed938eb0cb776dcd26e7b855c18418af088
sahalpanjwani15-source <sahalpanjwani15@gmail.com>
2026-08-25 21:14:20 +0500

    a

---
+++ sahal.py
@@ -0,0 +1,11 @@
+# part 1: ask today's temperature
+temperature = int(input("entere today's temperature in celsius : " ))
+if temperature < 20:
+    outfit ="jackit"
+    print("it is cold today.") 
+    print("waer a", outfit)
+else:
+    outfit = "t-shirt"
+    print("it is warm today.")
+    print("waer a", outfit)
+
