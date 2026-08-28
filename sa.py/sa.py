e8328fde3d8670fd03237446651555e9bf44ec47
sahalpanjwani15-source <sahalpanjwani15@gmail.com>
2026-08-29 02:06:24 +0500

    n

--- sa.py/sa.py
+++ sa.py/sa.py
@@ -1,1 +1,16 @@
+print("my daily routine")
 
+#step 1: waking up 
+print("I wake up  at 7 am.")
+
+#step 2: going o school 
+print("I go school at 8 am.")
+
+#step 3: playing after school 
+print("I play with my friends in the evening.")
+
+#step 4: studying 
+print("I study for 2 hours at night.")
+
+#calculating total study time in a week 
+print("total study hours in a week:", 2 * 7)
